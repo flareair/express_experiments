75e519d7193bc8ce582090b014ef2874a9993bb5
flareair <flareair0@gmail.com>
2014-12-23 12:19:04 +0300

    mongo connection ins separate module

--- app.js
+++ app.js
@@ -8,18 +8,6 @@ var app = express();
 
 require('./app/config/config')(express, app);
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/express');
-
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-
-db.once('open', function callback () {
-  console.log('DB Opened!');
-});
-
-
 require('./app/config/router')(app);
 
 
