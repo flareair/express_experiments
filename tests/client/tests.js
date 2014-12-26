54babebbfed33a92646e299f89171c8678dea065
flareair <flareair0@gmail.com>
2014-12-26 20:30:08 +0300

    Begin to write api factory tests

--- tests/client/tests.js
+++ tests/client/tests.js
@@ -6,14 +6,14 @@ describe('pageTitle factory', function() {
      factory = pageTitle;
   }));
 
-  it('Should return app name Microtalk by default', function() {
+  it('should return app name Microtalk by default', function() {
     factory.setTitle();
     var title = factory.getTitle();
     title.should.be.a.string;
     title.should.equal('Microtalk');
   });
 
-  it('Should set correct title', function() {
+  it('should set correct title', function() {
     var newTitle = 'foobar';
     factory.setTitle(newTitle);
     var title = factory.getTitle();
@@ -22,3 +22,37 @@ describe('pageTitle factory', function() {
   });
 });
 
+describe('api factory', function() {
+  var factory, $httpBackend;
+  beforeEach(module('App'));
+
+  beforeEach(inject(function(api, _$httpBackend_) {
+     factory = api;
+     $httpBackend = _$httpBackend_;
+  }));
+
+  afterEach(function () {
+    $httpBackend.verifyNoOutstandingRequest();
+    $httpBackend.verifyNoOutstandingExpectation();
+  });
+
+  it('should return right app data', function() {
+    var data = 'no data';
+
+    $httpBackend
+      .whenGET('api/appinfo')
+      .respond({name: 'foo', description: 'bar'});
+
+    factory.getAppData()
+      .then(function(res) {
+        data = res;
+      });
+
+    $httpBackend.flush();
+    data.should.be.a.object;
+    data.name.should.be.equal('foo');
+    data.description.should.be.equal('bar');
+
+
+  });
+});
